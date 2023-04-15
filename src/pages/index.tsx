75f42cb6af3f13571b2ad608506c26938d7434b0
henrique almeida <henrique.almeida231@hotmail.com>
2023-04-14 23:10:08 -0300

    fix: fix the microsoft clarity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,14 +59,10 @@ export default function Home() {
                         {
             __html: `
                 (function(c,l,a,r,i,t,y){
-                    c[a] = c[a] || function () { (c[a].q = c[a].q || 
-                    []).push(arguments) };
-                    t=l.createElement(r);
-                    t.async=1;
-                    t.src="https://www.clarity.ms/tag/"+i;
-                    y=l.getElementsByTagName(r)[0];
-                    y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "XXXXXXXXX");`,
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "gop9wgt9gr")`,
           }}
         />;
             
